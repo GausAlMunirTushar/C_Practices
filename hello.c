858b4b6e7162531bea413a15126346c6ab80b18a
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-03-20 10:47:44 +0600

    basic concept practics and two question solving

--- hello.c
+++ hello.c
@@ -1,10 +1,39 @@
 # include<stdio.h>
+// this code is for the hello world program and data types in C
+/* 
+This is multiline comment
+*/
 int main()
 {
-    int age = 21;
+    /* int age = 21;
     float height = 1.75;
-    char name = 'Gaus Al Munir Tushar';
+    char name = 'T';
     printf("Hello World\n");
+    printf("My name is %c\n", name); */
+
+    // Practice two number to sum
+   /*  int a,b;
+    printf("Enter a \n");
+    scanf("%d",&a);
+    printf("Enter b \n");
+    scanf("%d",&b);
+    int sum = a+b;
+    printf("Sum is %d\n",sum); */
+
+    // Practice QS 1 side of square;
+    /* int side;
+    printf("Enter side of square \n");
+    scanf("%d",&side);
+    int area = side*side;
+    printf("Area of square is %d\n",area); */
+
+    // Practice QS 2 area of circle;
+    int radius;
+    printf("Enter radius of circle \n");
+    scanf("%d",&radius);
+    float area = 3.14*radius*radius;
+    printf("Area of circle is %f\n",area);
+    
     return 0;
     
 }
