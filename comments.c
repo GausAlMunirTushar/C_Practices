4c68a5459fa67e963dd0904ee3c272cc1d737acb
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-03-25 16:22:41 +0600

    comments in c

--- comments.c
+++ comments.c
@@ -1,4 +1,15 @@
+/*********************************
+* Author: Gaus Al Munir Tushar
+* Purpose: To write first program
+**********************************/
 #include<stdio.h>
 void main(){
+    // this will print hello world using c
     printf("Hello World in C");
+
+    // this is single line comment in c
+
+    /* this is 
+    multiple 
+    line comment in c */
 }
